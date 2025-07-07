3e5f0eb56e33840f38e1b77ecd7617248fda67fa
Bela <27pvarga@gmail.com>
2025-07-07 16:41:49 -0400

    added map api controller

---
+++ LoungeSaber-Server/Api/Controllers/MapApiController.cs
@@ -0,0 +1,12 @@
+﻿using LoungeSaber_Server.SQL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoungeSaber_Server.Api.Controllers;
+
+[ApiController]
+public class MapApiController : ControllerBase
+{
+    [HttpGet("/api/maps/hashes")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<string[]> GetAllMapHashes() => MapData.Instance.GetAllMaps().Select(i => i.Hash).ToArray();
+}
