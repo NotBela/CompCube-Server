a99738ba0b8c035f1b90933d918edba73942a28e
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-07-06 20:35:18 -0700

    write some gamematch logic

---
+++ CompCube-Server/Interfaces/IDealer.cs
@@ -0,0 +1,10 @@
+﻿using CompCube_Models.Models.Map;
+
+namespace CompCube_Server.Interfaces;
+
+public interface IDealer
+{
+    public VotingMap[] PullNewCards(int count);
+
+    public void AddDiscarded(VotingMap[] disallowedMaps);
+}
