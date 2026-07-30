5b657f02eb9e57371f9bb706f6fad261861881c0
Bela <27pvarga@gmail.com>
2026-07-30 15:58:12 -0400

    another test message

--- CompCube-Server/Discord/MapPooling/BeatSaverSubmission/SubmitFromBeatsaverModalInteractionModule.cs
+++ CompCube-Server/Discord/MapPooling/BeatSaverSubmission/SubmitFromBeatsaverModalInteractionModule.cs
@@ -1,12 +1,17 @@
-﻿using NetCord.Services.ComponentInteractions;
+﻿using NetCord.Rest;
+using NetCord.Services.ComponentInteractions;
 
 namespace CompCube_Server.Discord.MapPooling;
 
 public class SubmitFromBeatsaverModalInteractionModule : ComponentInteractionModule<ModalInteractionContext>
 {
     [ComponentInteraction("submitMapFromBeatsaverModal")]
-    public void SubmitMapFromBeatsaverModalInteraction()
+    public async Task SubmitMapFromBeatsaverModalInteraction()
     {
+        await Context.Interaction.SendResponseAsync(InteractionCallback.Message("this will be deleted in 5 seconds lol"));
         
+        await Task.Delay(5000);
+
+        await Context.Interaction.DeleteResponseAsync();
     }
 }
