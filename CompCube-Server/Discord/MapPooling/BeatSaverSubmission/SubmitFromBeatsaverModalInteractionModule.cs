867729f0daf8112f23b01e09009c6097595cbd48
Bela <27pvarga@gmail.com>
2026-08-01 18:56:00 -0400

    update embed

--- CompCube-Server/Discord/MapPooling/BeatSaverSubmission/SubmitFromBeatsaverModalInteractionModule.cs
+++ CompCube-Server/Discord/MapPooling/BeatSaverSubmission/SubmitFromBeatsaverModalInteractionModule.cs
@@ -11,25 +11,27 @@ public class SubmitFromBeatsaverModalInteractionModule(IConfiguration config, Re
     [ComponentInteraction("submitMapFromBeatsaverModal")]
     public async Task SubmitMapFromBeatsaverModalInteraction()
     {
+        await SendEmptyResponse();
+        
         var category = Context.Components.OfType<Label>().Select(i => i.Component).OfType<StringMenu>().First().SelectedValues?.FirstOrDefault();
         var bsr = Context.Components.OfType<Label>().Select(i => i.Component).OfType<TextInput>().FirstOrDefault()?.Value;
         var difficulty = Context.Components.OfType<Label>().Select(i => i.Component).OfType<StringMenu>().Last().SelectedValues?.FirstOrDefault();
 
         if (category is null)
         {
-            await RespondUsingDms("Failed to get category from form!");
+            await DmUser("Failed to get category from form!");
             return;
         }
 
         if (bsr is null)
         {
-            await RespondUsingDms("Failed to get BSR from form!");
+            await DmUser("Failed to get BSR from form!");
             return;
         }
 
         if (difficulty is null)
         {
-            await RespondUsingDms("Failed to fetch difficulty from form!");
+            await DmUser("Failed to fetch difficulty from form!");
             return;
         }
 
@@ -37,19 +39,19 @@ public class SubmitFromBeatsaverModalInteractionModule(IConfiguration config, Re
 
         if (beatSaverBeatmap is null)
         {
-            await RespondUsingDms($"Failed to fetch beatmap {bsr} from beatsaver! (Are you sure this is the right key?)");
+            await DmUser($"Failed to fetch beatmap {bsr} from beatsaver! (Are you sure this is the right key?)");
             return;
         }
         
         var poolingChannelId = config.GetSection("Discord").GetSection("PoolingChannelIds").GetValue<ulong>(category);
 
-        var forumThread = await client.CreateForumGuildThreadAsync(poolingChannelId, new ForumGuildThreadProperties(beatSaverBeatmap.Name, new ForumGuildThreadMessageProperties
+        var forumThread = await client.CreateForumGuildThreadAsync(poolingChannelId, new ForumGuildThreadProperties($"({beatSaverBeatmap.Metadata.LevelAuthorName}) {beatSaverBeatmap.Name}", new ForumGuildThreadMessageProperties
         {
             Embeds = [new EmbedProperties
             {
                 Image = new EmbedImageProperties(beatSaverBeatmap.LatestVersion.CoverURL),
                 Title = beatSaverBeatmap.Name,
-                Description = beatSaverBeatmap.Description,
+                // Description = beatSaverBeatmap.Description,
                 Fields = [new EmbedFieldProperties
                     {
                         Name = "Map Link",
@@ -58,27 +60,38 @@ public class SubmitFromBeatsaverModalInteractionModule(IConfiguration config, Re
                     new EmbedFieldProperties
                     {
                         Name = "Difficulty",
-                        Value = difficulty
+                        Value = difficulty,
+                        Inline = true,
                     },
                     new EmbedFieldProperties()
                     {
                         Inline = true,
+                        Name = "Submitted by:",
+                        Value = $"<@{Context.User.Id}>"
+                    },
+                    new EmbedFieldProperties()
+                    {
+                        Inline = false,
                         Name = "Category",
                         Value = category
-                    }
+                    },
+                    new EmbedFieldProperties()
+                    {
+                        Inline = false,
+                        Name = "Mapper",
+                        Value = $"{beatSaverBeatmap.Metadata.LevelAuthorName} ({beatSaverBeatmap.Uploader.Name})"
+                    },
                 ]
             }]
         }));
-
-        await SendEmptyResponse();
+        
+        
     }
 
     private async Task SendEmptyResponse() => await RespondAsync(InteractionCallback.DeferredModifyMessage);
 
-    private async Task RespondUsingDms(MessageProperties message)
+    private async Task DmUser(MessageProperties message)
     {
-        await RespondAsync(InteractionCallback.DeferredModifyMessage);
-
         var dmChannel = await Context.User.GetDMChannelAsync();
 
         await dmChannel.SendMessageAsync(message);
