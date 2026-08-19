73bfc0f93ccbd18e8ffec828c9d070c3739d50ee
Bela <27pvarga@gmail.com>
2026-08-19 02:25:49 -0400

    add whitelist functionality

--- CompCube-Server/Config/ConfigHelper.cs
+++ CompCube-Server/Config/ConfigHelper.cs
@@ -5,4 +5,8 @@ public class ConfigHelper(IConfiguration config)
     public int Season => config.GetSection("Gameplay").GetValue("Season", 0);
 
     public string Secret => config.GetSection("Api").GetValue<string>("Secret")!;
+    
+    public bool WhitelistEnabled => config.GetSection("Whitelist").GetValue("Enabled", false);
+    
+    public string[] WhitelistedIds => config.GetSection("Whitelist").GetSection("AllowedIds").Get<string[]>() ?? [];
 }
--- CompCube-Server/Gameplay/Matchmaking/ConnectionManager.cs
+++ CompCube-Server/Gameplay/Matchmaking/ConnectionManager.cs
@@ -3,6 +3,7 @@ using System.Text;
 using CompCube_Models.Models.Packets;
 using CompCube_Models.Models.Packets.ServerPackets;
 using CompCube_Models.Models.Packets.UserPackets;
+using CompCube_Server.Config;
 using CompCube_Server.Data;
 using CompCube_Server.Interfaces;
 using CompCube_Server.Networking.Client;
@@ -16,16 +17,18 @@ public class ConnectionManager
     private readonly ILogger<ConnectionManager> _logger;
     private readonly ClientFactory _clientFactory;
     private readonly TimeoutManager _timeoutManager;
+    private readonly ConfigHelper _config;
     
     private readonly List<IConnectedClient> _connectedClients = [];
     
-    public ConnectionManager(UserData userData, ILogger<ConnectionManager> logger, QueueManager queueManager, ClientFactory clientFactory, TimeoutManager timeoutManager)
+    public ConnectionManager(UserData userData, ILogger<ConnectionManager> logger, QueueManager queueManager, ClientFactory clientFactory, TimeoutManager timeoutManager, ConfigHelper config)
     {
         _userData = userData;
         _logger = logger;
         _queueManager = queueManager;
         _clientFactory = clientFactory;
         _timeoutManager = timeoutManager;
+        _config = config;
 
         Task.Factory.StartNew(PollAllClients, TaskCreationOptions.LongRunning);
         _logger.LogInformation("Started listening for clients");
@@ -74,11 +77,21 @@ public class ConnectionManager
             socketFinishedTcs.SetResult();
             return;
         }
+
+        if (_config.WhitelistEnabled && !_config.WhitelistedIds.Contains(joinRequestPacket.UserId))
+        {
+            var bytes = new  JoinResponsePacket(false, "You are not whitelisted!").SerializeToBytes();
+            
+            await socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            await socket.CloseOutputAsync(WebSocketCloseStatus.InternalServerError, "", CancellationToken.None);
+            socketFinishedTcs.SetResult();
+            return;
+        }
         
         var userInfo = _userData.UpdateUserDataOnLogin(joinRequestPacket.UserId, joinRequestPacket.UserName);
 
         var client = _clientFactory.Create(userInfo, socket, socketFinishedTcs);
-
+        
         if (client.UserInfo.Banned)
         {
             await client.SendPacket(new JoinResponsePacket(false, "You have been banned from CompCube"));
